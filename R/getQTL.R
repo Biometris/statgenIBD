70c862e98c10bc929f05d5b4e79e2d208794b161
Rossum, Bart-Jan van <bart-jan.vanrossum@wur.nl>
2020-05-27 16:52:17 +0200

    Added first version of getQTL

---
+++ R/getQTL.R
@@ -0,0 +1,23 @@
+#' @export
+getQTL <- function(IBD,
+                   QTLS) {
+  ## Checks.
+  if (!inherits(IBD, "statgenIBD")) {
+    stop(deparse(substitute(IBD)), "should be an object of class statgenIBD\n")
+  }
+  if (is.null(QTLS) || !is.character(QTLS)) {
+    stop("QTLS should be a character vector\n")
+  }
+  missQTL <- QTLS[!QTLS %in% rownames(IBD$markers)]
+  if (length(missQTL) > 0) {
+    stop("The following QTLs are not in ", deparse(substitute(IBD)), ": ",
+         paste(missQTL, collapse = ", "), "\n")
+  }
+  markerQTLS <- lapply(X = QTLS, FUN = function(QTL) {
+    markerQTL <- IBD$markers[QTL, , ]
+    colnames(markerQTL) <- paste0(colnames(markerQTL), "_", QTL)
+    return(markerQTL)
+  })
+  markerQTLS <- do.call(cbind, markerQTLS)
+  return(markerQTLS)
+}
