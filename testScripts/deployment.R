24f36b0470959dc908ab97d6abcd2afb8ef2d8c4
Rossum, Bart-Jan van <bart-jan.vanrossum@wur.nl>
2021-07-21 11:27:42 +0200

    Added test infrastructure

---
+++ testScripts/deployment.R
@@ -0,0 +1,16 @@
+## Check with winbuilder - develop only.
+devtools::check_win_devel()
+
+## Check with rhub.
+rhub::check_for_cran(env_vars = c(`_R_CHECK_FORCE_SUGGESTS_` = "false"))
+
+## Rebuild readme.
+devtools::build_readme()
+
+## Submit to CRAN
+devtools::release()
+
+
+## Build site.
+pkgdown::clean_site()
+pkgdown::build_site()
