50c7d6608e41bf5cafa44c6d92d42a1f31c51a3c
Bart-Jan van Rossum <bart-jan.vanrossum@wur.nl>
2022-08-08 12:34:01 +0200

    Updated tests

--- inst/tinytest/test_readIBDs.R
+++ inst/tinytest/test_readIBDs.R
@@ -4,11 +4,11 @@
 ibdFile <- system.file("extdata/SxM", "SxM_IBDs.ibd", package = "statgenIBD")
 
 ## Checks for correct input.
-expect_error(readIBDs(file = 1), "File path should be a single character string.")
-expect_error(readIBDs(file = "a/b.txt"), "File not found.")
+expect_error(readIBDs(inFile = 1), "File path should be a single character string.")
+expect_error(readIBDs(inFile = "a/b.txt"), "File not found.")
 
 ## Check for successful read and returned structure.
-expect_silent(ibds <- readIBDs(file = ibdFile))
+expect_silent(ibds <- readIBDs(inFile = ibdFile))
 expect_inherits(ibds, "IBDprob")
 expect_inherits(ibds$markers, "array")
 expect_equal(dim(ibds$markers), c(116, 150, 2))
